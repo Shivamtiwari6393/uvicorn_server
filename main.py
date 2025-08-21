283cace427e6e1775caec5d9e79b97f8a2fcfe88
Shivam Tiwari <shivamtiwari6223@gmail.com>
2025-08-21 16:56:32 +0530

    removed print

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
 import tensorflow as tf
 import keras
-from keras.models import load_model 
+from keras.models import load_model
 import cv2
 
 
@@ -54,29 +54,30 @@ async def upload_image(
 async def predict_image(
     image: UploadFile = File(...)
 ):
-
+    # print("predicting")
     contents = await image.read()
     npimg = np.frombuffer(contents, np.uint8)
     img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
 
-    # img = cv2.imread('C:/Users/91639/Pictures/python_model/augmented_dataset/B/B_0_1018.jpg')
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    img = cv2.resize(img, (128,128))
-    img = img.reshape(-1, 128,128,3)
-    # cv2.imshow(img)
+    img = cv2.resize(img, (128, 128))
+
+    img = img.reshape(-1, 128, 128, 3)
 
     # Predict
     pred = model.predict(img)
     predicted_class = pred.argmax()
-    classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'del', 'nothing', 'space']
+    classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+               'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'del', 'nothing', 'space']
 
     out = classes[predicted_class]
     accu = pred[0][predicted_class]
 
-    print(out, accu)
+    # print(out, accu)
 
     return JSONResponse(
-        content={"message": f"Prediction successful acc {accu*100}", "filename": out},
+        content={"message": f"Prediction successful acc {accu*100}",
+                 "filename": out},
         status_code=200
     )
